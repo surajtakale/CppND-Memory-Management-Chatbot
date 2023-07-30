c4eb241c4a332767a6be966e8ddddc815208b84a
Suraj <suraj.takale@elastic.run>
2023-07-30 13:03:45 +0530

    updates

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -29,7 +29,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
     _childEdges.push_back(edge);
 }
